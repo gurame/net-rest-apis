82c52c24473fdd38b1b23ecfadcae2e1a36c5437
Gustavo Rabanal <15724234+gurame@users.noreply.github.com>
2024-08-06 18:22:46 -0500

    feat: add authentication to swagger

--- src/Movies.Api/Swagger/ConfigureSwaggerOptions.cs
+++ src/Movies.Api/Swagger/ConfigureSwaggerOptions.cs
@@ -29,29 +29,29 @@ public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
                 });
         }
         
-        // options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-        // {
-        //     In = ParameterLocation.Header,
-        //     Description = "Please provide a valid token",
-        //     Name = "Authorization",
-        //     Type = SecuritySchemeType.Http,
-        //     BearerFormat = "JWT",
-        //     Scheme = "Bearer"
-        // });
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please provide a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
         
-        // options.AddSecurityRequirement(new OpenApiSecurityRequirement
-        // {
-        //     {
-        //         new OpenApiSecurityScheme
-        //         {
-        //             Reference = new OpenApiReference
-        //             {
-        //                 Type = ReferenceType.SecurityScheme,
-        //                 Id = "Bearer"
-        //             }
-        //         },
-        //         Array.Empty<string>()
-        //     }
-        // });
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
     }
 }
